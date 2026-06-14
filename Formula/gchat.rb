7398aa0c87b01e51219fb4af871b68bcfdb4e212
Kamil Rudnicki <kamil.rudnicki@gmail.com>
2026-06-14 15:37:10 +0200

    Add Linux formula URLs

--- Formula/gchat.rb
+++ Formula/gchat.rb
@@ -14,6 +14,16 @@ class Gchat < Formula
     end
   end
 
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/kamil-rudnicki/google-chat-cli/releases/download/v1.0.0/gchat-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "73d4c1ca2a8d1c41ff769791fd8d8e9c4de0f099af97d0a9772d2485057cf4e5"
+    else
+      url "https://github.com/kamil-rudnicki/google-chat-cli/releases/download/v1.0.0/gchat-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "ad2f97c74cc47a075718fc78515a31dd75077e424c5c86f94859434e1d21246b"
+    end
+  end
+
   def install
     bin.install "gchat"
   end
